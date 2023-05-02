696dc255630a4b80d0972f50d4bca556979253f7
Darshak <darshakpatel02@gmail.com>
2023-05-02 13:02:03 +0530

    login photo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 
-<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default"
-    data-assets-path="../../assets/" data-template="vertical-menu-template">
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../../assets/" data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>KWV</title>
 
@@ -18,9 +16,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
     <!-- Icons -->
     <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
@@ -57,8 +53,9 @@
     <div align="center">
 
         <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
-            <div class="w-px-400 mx-auto">
+            <div class="w-px-400 mx-auto"><br>
                 <img src="{{url('/image/kwv.jpg')}}" alt="Image" width="150" height="150" />
+                <br><br>
                 <h3 class="mb-1 fw-bold">Welcome to KWV! 👋</h3>
                 <p class="mb-4">Please sign-in to your account and start the adventure</p>
                 @if (\Session::has('error'))
@@ -72,8 +69,7 @@
                     @csrf
                     <div class="mb-3">
                         <label for="phone" class="form-label">Phone Number or Username</label>
-                        <input id="phone" type="text" class="form-control " name="phone" value="{{ old('phone') }}"
-                            placeholder="Enter your phone number or username" required autofocus />
+                        <input id="phone" type="text" class="form-control " name="phone" value="{{ old('phone') }}" placeholder="Enter your phone number or username" required autofocus />
                         <!-- @if ($errors->has('phone'))
                         <span class="help-block">
                             <strong>{{ $errors->first('phone') }}</strong>
@@ -91,10 +87,7 @@
 
                         </div>
                         <div class="input-group input-group-merge">
-                            <input id="password" type="password"
-                                class="form-control @error('password') is-invalid @enderror" name="password"
-                                placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                required autocomplete="current-password" />
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" required autocomplete="current-password" />
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
