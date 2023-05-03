13574401e3675a31015859cec3980cb65a214c4e
Darshak <darshakpatel02@gmail.com>
2023-05-03 16:47:19 +0530

    app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,15 @@
 
 <head>
     <meta charset="utf-8" />
-    <!-- @if(auth()->user()->is_admin == 1)
+    @if(auth()->user()->is_admin == 1)
     <title>Admin Dashboard</title>
     @else
     <title>Employee Dashboard</title>
-    @endif -->
-    <title>KWV</title>
+    @endif
 
-    <meta name="description" content="" />
-
+    <!-- <meta name="description" content="" /> -->
+    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="/assets/img/favicon/kwv.ico" />
 
